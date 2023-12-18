e44bf8fff8366267c7c753478d1c16c1933e776b
IlliamyrVolkov <ipvolkov.fitm22@kubg.edu.ua>
2023-12-19 01:00:15 +0200

    Initial commit

---
+++ Task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введіть довжину першої сторони: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть довжину другої сторони: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+            
+            Rectangle rectangle = new Rectangle(side1, side2);
+            
+            Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter}");
+            Console.WriteLine($"Площа прямокутника: {rectangle.Area}");
+        }
+    }
+}
---
+++ Task_02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Point point1 = new Point(0, 0, "A");
+            Point point2 = new Point(0, 1, "B");
+            Point point3 = new Point(1, 0, "C");
+            
+            Figure triangle = new Figure(point1, point2, point3);
+
+            triangle.CalculatePerimeter();
+        }
+    }
+}
